478704250eeb442b560c16803739e83ca42f21da
yammyy <yammyy01@gmail.com>
2026-01-22 16:03:06 +0200

    BcatNovel is child of madara, so DOM is another then madara

--- .js/src/plugins/index.js
+++ .js/src/plugins/index.js
@@ -45,7 +45,7 @@ var StoriesEcho_madara_1 = __importDefault(require("@plugins/english/StoriesEcho
 var WordRain_madara_1 = __importDefault(require("@plugins/english/WordRain[madara]"));
 var ClownCo_madara_1 = __importDefault(require("@plugins/english/ClownCo[madara]"));
 var BelleRepository_madara_1 = __importDefault(require("@plugins/english/BelleRepository[madara]"));
-var BcatNovel_madara_1 = __importDefault(require("@plugins/english/BcatNovel[madara]"));
+var BcatNovel_1 = __importDefault(require("@plugins/english/BcatNovel"));
 var NovelLib_fictioneer_1 = __importDefault(require("@plugins/english/NovelLib[fictioneer]"));
 var SyosetuW_1 = __importDefault(require("@plugins/japanese/SyosetuW"));
 var SyosetuM_1 = __importDefault(require("@plugins/japanese/SyosetuM"));
@@ -94,7 +94,7 @@ var PLUGINS = [
     ClownCo_madara_1.default,
     BelleRepository_madara_1.default,
     NovelLib_fictioneer_1.default,
-    BcatNovel_madara_1.default,
+    BcatNovel_1.default,
     SyosetuW_1.default,
     SyosetuM_1.default,
     SyosetuG_1.default,
