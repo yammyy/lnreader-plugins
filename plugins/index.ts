643a106a1a3aeee75e0f155b25bbfd8e30873197
yammyy <yammyy01@gmail.com>
2026-01-27 14:31:40 +0200

    Tuning

--- plugins/index.ts
+++ plugins/index.ts
@@ -45,7 +45,7 @@ import p_30009 from '@plugins/english/ClownCo[madara]';
 import p_30010 from '@plugins/english/BelleRepository[madara]';
 import p_30011 from '@plugins/english/NovelLib[fictioneer]';
 import p_30012 from '@plugins/english/BcatNovel[madara-child]';
-import p_30013 from '@plugins/english/StoriesRealm[madara-child]';
+import p_30013 from '@plugins/english/StoriesRealm[madara]';
 import p_40001 from '@plugins/japanese/SyosetuW';
 import p_40002 from '@plugins/japanese/SyosetuM';
 import p_40003 from '@plugins/japanese/SyosetuG';
